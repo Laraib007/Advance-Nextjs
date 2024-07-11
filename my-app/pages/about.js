bded0e3165282d0e963fff0b3860d4040a38ccf6
Laraib007 <lrbkhan01@gmail.com>
2024-07-11 20:21:00 +0500

    using style css in global

--- my-app/pages/about.js
+++ my-app/pages/about.js
@@ -1,7 +1,13 @@
 export default function About(){
     return(
         <>
-       
+        <style jsx global>
+      {`
+        .span1{
+        color: green;
+        }
+      `}
+        </style>
         <div>This is About Page</div>
         </>
     )
