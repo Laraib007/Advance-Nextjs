9f595090eb2dcd822395513a2a0109dc283a8e4b
Laraib007 <lrbkhan01@gmail.com>
2024-07-11 19:44:54 +0500

    creating custom css file

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import styles1 from "../styles/Home1.module.css";
 import Script from "next/script";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
@@ -15,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className={styles.mainNav}> 
+      <nav className={`${styles.mainNav} ${styles1.con}`}> 
           <ul>
           <Link href=""><li>Home</li></Link>
            <Link href="/about"> <li>About</li></Link>
