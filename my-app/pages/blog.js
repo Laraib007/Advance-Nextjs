5ed6b9c4c3df35352023666a244571d27f3b4b99
Laraib007 <lrbkhan01@gmail.com>
2024-07-12 19:04:46 +0500

    adding blogs page

--- my-app/pages/blog.js
+++ my-app/pages/blog.js
@@ -1,5 +1,35 @@
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+
+
 export default function Blog (){
     return(
-        <div>This is Blog Page</div>
+        <main className={`${styles.main} ${inter.className}`}>
+        <div> <div className="blog">
+        <div className="blogItems">
+          <h3>How to Learn javascript in 2024?</h3>
+          <p>This is a blog to learn javascript from starting to advance</p>
+        </div>
+      </div>
+      <div className="blog">
+        <div className="blogItems">
+          <h3>How to Learn javascript in 2024?</h3>
+          <p>This is a blog to learn javascript from starting to advance</p>
+        </div>
+      </div>
+      <div className="blog">
+        <div className="blogItems">
+          <h3>How to Learn javascript in 2024?</h3>
+          <p>This is a blog to learn javascript from starting to advance</p>
+        </div>
+      </div>
+      <div className="blog">
+        <div className="blogItems">
+          <h3>How to Learn javascript in 2024?</h3>
+          <p>This is a blog to learn javascript from starting to advance</p>
+        </div>
+      </div>
+</div>
+</main>
     )
 }
