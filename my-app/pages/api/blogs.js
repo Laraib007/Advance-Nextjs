9fc3c6cb75be63f732036761baf86e663dda3a52
Laraib007 <lrbkhan01@gmail.com>
2024-07-12 20:41:10 +0500

    using fs to import json file

---
+++ my-app/pages/api/blogs.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs'
+export default function handler(req, res) {
+  fs.readFile("blogdata/learn.json", 'utf-8', (err, data)=>{ console.log(data)
+    res.status(200).json(JSON.parse(data));
+  })
+ 
+  
+}
