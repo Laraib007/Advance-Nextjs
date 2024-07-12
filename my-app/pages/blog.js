85d442d2d2c4f6d22165ab845d20ef39f440314e
Laraib007 <lrbkhan01@gmail.com>
2024-07-12 19:24:44 +0500

    changing font

--- my-app/pages/blog.js
+++ my-app/pages/blog.js
@@ -1,13 +1,14 @@
 import styles from "@/styles/Home.module.css";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 
 export default function Blog (){
     return(
-        <main className={`${styles.main} ${inter.className}`}>
+        <main className={`${styles.main} `}>
         <div> <div className="blog">
         <div className="blogItems">
-          <h3>How to Learn javascript in 2024?</h3>
+          <h3> <Link href="/blogpost/javascript-learing"> How to Learn javascript in 2024?</Link></h3>
           <p>This is a blog to learn javascript from starting to advance</p>
         </div>
       </div>
