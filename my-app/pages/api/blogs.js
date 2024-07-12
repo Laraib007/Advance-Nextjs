81a7880ebe6f7de79cb1dd6ace7ef2111993eb0d
Laraib007 <lrbkhan01@gmail.com>
2024-07-12 22:17:00 +0500

    fatching APIs

--- my-app/pages/api/blogs.js
+++ my-app/pages/api/blogs.js
@@ -1,12 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs'
-export default function handler(req, res) {
-  fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data)=>{ console.log(data)
-    if(err){
-      res.status(500).json({error: "No such file found"});
-    }
-    res.status(200).json(JSON.parse(data));
-  })
- 
-  
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata")
+let myfiles;
+let allBlogs = [];
+for(let i = 0; i < data.length; i++){
+  const items = data[i]
+   myfiles = await fs.promises.readFile((`blogdata/`+ items), "utf-8")
+  allBlogs.push(JSON.parse(myfiles))
+}
+res.status(200).json(allBlogs)
+
+
+
+
+
 }
--- my-app/pages/blog.js
+++ my-app/pages/blog.js
@@ -1,36 +1,33 @@
 import styles from "@/styles/Home.module.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 
 export default function Blog (){
+  const [blog, setBlog] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/api/blogs").then((a)=>{
+      return a.json()})
+      .then((parsed)=>{
+        setBlog(parsed)
+      })
+  },[])
+
     return(
         <main className={`${styles.main} `}>
-        <div> <div className="blog">
+          {blog.map((blogD)=>{
+            return <>
+             <div> <div className="blog">
         <div className="blogItems">
-          <h3> <Link href="/blogpost/javascript-learing"> How to Learn javascript in 2024?</Link></h3>
-          <p>This is a blog to learn javascript from starting to advance</p>
+          <h3> <Link href={`/blogpost/${blogD.slug}`}>{blogD.title}</Link></h3>
+          <p>{blogD.des}</p>
+          <h4>By Mr.{blogD.author}</h4>
         </div>
       </div>
-      <div className="blog">
-        <div className="blogItems">
-          <h3>How to Learn javascript in 2024?</h3>
-          <p>This is a blog to learn javascript from starting to advance</p>
-        </div>
       </div>
-      <div className="blog">
-        <div className="blogItems">
-          <h3>How to Learn javascript in 2024?</h3>
-          <p>This is a blog to learn javascript from starting to advance</p>
-        </div>
-      </div>
-      <div className="blog">
-        <div className="blogItems">
-          <h3>How to Learn javascript in 2024?</h3>
-          <p>This is a blog to learn javascript from starting to advance</p>
-        </div>
-      </div>
-</div>
+            </>
+          })}
 </main>
     )
 }
