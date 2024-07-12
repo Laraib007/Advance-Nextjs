fe1b98ef3670e4e6c273a0b22bbde56088c9b000
Laraib007 <lrbkhan01@gmail.com>
2024-07-12 20:51:36 +0500

    throughing error if error found

--- my-app/pages/api/blogs.js
+++ my-app/pages/api/blogs.js
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs'
 export default function handler(req, res) {
-  fs.readFile("blogdata/learn.json", 'utf-8', (err, data)=>{ console.log(data)
+  fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data)=>{ console.log(data)
+    if(err){
+      res.status(500).json({error: "No such file found"});
+    }
     res.status(200).json(JSON.parse(data));
   })
  
