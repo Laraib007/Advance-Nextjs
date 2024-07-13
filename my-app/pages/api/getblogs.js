fc883fc2e52cdb0636301f6ae5d2841f3790b32a
Laraib007 <lrbkhan01@gmail.com>
2024-07-13 11:55:13 +0500

    creating api

---
+++ my-app/pages/api/getblogs.js
@@ -0,0 +1,10 @@
+import fs from 'fs'
+export default async function handler(req, res) {
+   fs.readFile((`blogdata/${req.query.slug}.json`), "utf-8", (err, data)=>{
+    if(err){
+        res.status(500).json({err: "No such blog found"})
+    }
+    res.status(200).json(JSON.parse(data))
+   })
+}
+
--- my-app/pages/blogpost/[slug].js
+++ my-app/pages/blogpost/[slug].js
@@ -3,27 +3,25 @@ import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
 
 export default function Slug (){
-    const router = useRouter()
-    
     const [blog, setBlog] = useState([])
+    const router = useRouter()
   useEffect(()=>{
     if(!router.isReady) return
     const {slug} = router.query
-    fetch(`http://localhost:3000/api/blogs?slug=${slug}`).then((a)=>{
+    fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((a)=>{
       return a.json()})
       .then((parsed)=>{
         setBlog(parsed)
       })
-  },[])
+  },[router.isReady])
     return(
         <main className={`${styles.main}`}>
         <div>
         <h1 className={styles.center}>
-        <span className="span1">Title of the Page </span>
+        <span className="span1">{blog && blog.title} </span>
         </h1>
-        <p className={`${styles.container} ${styles.main}`}>The Gemz daily combo 12 July is a cool part of the Gemz game for players who want to earn 5,000,000 coins each day. To get today’s rewards, players need to open four cards from the game. You can now get the Gemz Daily Combo 12 July Card from here.
-
-</p>
+        <p className={`${styles.container} `}>{blog && blog.des}</p>
+        <p className={`${styles.container} `}>{blog && blog.author}</p>
         </div>
         </main>
     )
