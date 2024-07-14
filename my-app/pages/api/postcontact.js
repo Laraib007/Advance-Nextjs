9327648844bbb3127289942f5e875c1f837420fc
Laraib007 <lrbkhan01@gmail.com>
2024-07-14 16:51:12 +0500

    creating unlimited json files using api req to wirite in body

--- my-app/pages/api/postcontact.js
+++ my-app/pages/api/postcontact.js
@@ -2,7 +2,8 @@ import fs from 'fs'
 
 export default async function handler(req, res) {
     if(req.method === "POST"){
-        fs.writeFile("contactdata/1.json", JSON.stringify(req.body), ()=>{})
+       let data = await fs.promises.readdir("contactdata")
+       fs.promises.writeFile(`contactdata/${data.length +1}.json`, JSON.stringify(req.body), ()=>{})
         return res.status(200).json(req.body)
     }
     res.status(400).json({err: "Sorry this is not a valid req"})
