f24e767140b542f706db548eafb12245fba0a47a
Laraib007 <lrbkhan01@gmail.com>
2024-07-16 15:25:01 +0500

    adding services to about page

--- my-app/pages/about.js
+++ my-app/pages/about.js
@@ -1,7 +1,21 @@
 export default function About(){
     return(
         <>
-        <div>This is About Page</div>
+        <div>
+            <h1>About Hunting Coder</h1>
+            <h2>Introduction</h2>
+            <p>As discussed, we know that HTTP errors must be handled by using the response object properties Response.ok and Response.status. Response.ok is a Boolean value that lets us know if the response was successful or not, whereas Response.status represents the HTTP status codes, such as 200 for success and 404 if the resource is not found. Have a look at the following response object:</p>
+            <h2>Services Offered</h2>
+            <p>As discussed, we know that HTTP errors must be handled by using the response object properties Response.ok and Response.status. Response.ok is a Boolean value that lets us know if the response was successful or not, whereas Response.status represents the HTTP status codes, such as 200 for success and 404 if the resource is not found. Have a look at the following response object:</p>
+            <p>What Services we offer?</p>
+            <ul>
+                <li>Service 1</li>
+                <li>Service 1</li>
+                <li>Service 1</li>
+                <li>Service 1</li>
+                <li>Service 1</li>
+            </ul>
+        </div>
         </>
     )
 }
