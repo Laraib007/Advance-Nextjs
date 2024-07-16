709ea41f6eab8f2003c54128779ca1590b860bfd
Laraib007 <lrbkhan01@gmail.com>
2024-07-16 15:27:15 +0500

    adding styles to about page

--- my-app/pages/about.js
+++ my-app/pages/about.js
@@ -1,7 +1,9 @@
+import styles from "@/styles/About.module.css";
+
 export default function About(){
     return(
         <>
-        <div>
+        <div className={styles.container}>
             <h1>About Hunting Coder</h1>
             <h2>Introduction</h2>
             <p>As discussed, we know that HTTP errors must be handled by using the response object properties Response.ok and Response.status. Response.ok is a Boolean value that lets us know if the response was successful or not, whereas Response.status represents the HTTP status codes, such as 200 for success and 404 if the resource is not found. Have a look at the following response object:</p>
